14b2c79d1c2d12effecc141f0037fde183444143
juliodigia <57037770+juliodigia@users.noreply.github.com>
2019-11-01 00:02:53 -0300

    Add files via upload

---
+++ AhorcadoClass.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AhorcadoClass
+{
+    public class AhorcadoClass
+    {
+        public AhorcadoClass()
+        {
+            palabra = "caracoles";
+            AsignarPalabra(palabra);
+
+        }
+
+        public AhorcadoClass(int Modo)
+        {
+            LetrasErradas = "      ";
+            ContErrores = 0;
+
+        }
+
+        public AhorcadoClass(string palabra)
+        {
+            AsignarPalabra(palabra);
+            LetrasErradas = "      ";
+            ContErrores = 0;
+        }
+
+        private DateTime inicio;
+        public DateTime Inicio
+        {
+            get { return this.inicio; }
+            set { this.inicio = value; }
+        }
+
+        private DateTime fin;
+        public DateTime Fin
+        {
+            get { return this.fin; }
+            set { this.fin = value; }
+        }
+
+        static int indiceLetraErrada = 1;
+        private string letrasErradas;
+        public string LetrasErradas
+        {
+            get { return this.letrasErradas; }
+            set { this.letrasErradas = value; }
+        }
+
+        private int contErrores;
+        public int ContErrores
+        {
+            get
+            {
+                return this.contErrores;
+            }
+            set
+            {
+                this.contErrores = value;
+            }
+        }
+
+        private string nombre;
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+        private string palabraAdivinada;
+        public string PalabraAdivinada
+        {
+            get { return this.palabraAdivinada; }
+            set { this.palabraAdivinada = value; }
+        }
+        private string palabra;
+        public string Palabra
+        {
+            get { return this.palabra; }
+            set { this.palabra = value; }
+        }
+        public void AsignarNombre(string x)
+        {
+
+            this.Nombre = x;
+        }
+
+        public string AsignarPalabraRandom()
+        {
+            string[] todasLasPalabras = new string[10] { "mierda", "extravagante", "epsilon", "grandioso", "sol", "calefactor", "puerta", "golpe", "sillon", "esdrujula" };
+            Random rm = new Random();
+            int nro = rm.Next(1, 10);
+            Palabra = todasLasPalabras[nro];
+            return Palabra;
+
+        }
+
+        public void AsignarPalabra(string x)
+        {
+
+            this.Palabra = x;
+            for (int i = 0; i <= Palabra.Length - 1; i++)
+            {
+                if (Palabra.Substring(i, 1) != " ")
+                {
+                    PalabraAdivinada = PalabraAdivinada + "-";
+
+                }
+                else
+                {
+                    PalabraAdivinada = PalabraAdivinada + " ";
+                }
+            }
+        }
+
+        public int CantLetras()
+        {
+            return palabra.Length;
+        }
+
+        public string IngresarLetra(string letra)
+        {
+
+            if (Palabra.Contains(letra))
+            {
+
+                for (int i = 0; i <= Palabra.Length - 1; i++)
+                {
+                    string letraAux = Palabra.Substring(i, 1);
+                    if (letra == letraAux)
+                    {
+                        PalabraAdivinada = PalabraAdivinada.Remove(i, 1);
+                        PalabraAdivinada = PalabraAdivinada.Insert(i, letra);
+                    }
+                }
+
+                if (PalabraAdivinada.Contains("-"))
+                {
+                    return PalabraAdivinada;
+                }
+                else
+                {
+                    return "Palabra adivinada";
+                }
+            }
+            else
+            {
+
+                LetrasErradas = LetrasErradas.Insert(indiceLetraErrada, letra);
+                indiceLetraErrada++;
+                ContErrores++;
+                if (ContErrores == 6) { return "Perdiste, queso"; }
+                return "letra incorrecta";
+            }
+
+
+        }
+
+
+
+        public bool ValidarPalabra()
+        {
+            foreach (var letra in Palabra.ToCharArray())
+            {
+                if (!PalabraAdivinada.Contains(Char.ToLower(letra)))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public double CalcularTiempoJuego(DateTime ini, DateTime fin)
+        {
+            var segundos = (fin - ini).TotalSeconds;
+            return segundos;
+        }
+
+        public string ArriesgarPalabra(string x)
+        {
+            string palabraArriesgada = x;
+            if (Palabra == palabraArriesgada)
+            {
+                return "arriesgaste bien, campeon";
+            }
+            else return "te apuraste y te la mandaste, perdedor";
+        }
+
+
+
+
+    }
+}
---
+++ AhorcadoTest.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AhorcadoClass;
+
+
+namespace Ahorcado
+{
+    [TestClass]
+    public class AhorcadoTest
+    {
+        [TestMethod]
+        public void TestIngresarNombre()
+        {
+            AhorcadoClass.AhorcadoClass ahorcadoGame = new AhorcadoClass.AhorcadoClass();
+            string nombre = "Manuel";
+            ahorcadoGame.AsignarNombre(nombre);
+            Assert.AreEqual(nombre, ahorcadoGame.Nombre);
+
+        }
+
+        [TestMethod]
+        public void TestUnaPalabra()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            string palabra = "caracoles";
+            ahoracadoGame.AsignarPalabra(palabra);
+            Assert.AreEqual(palabra, ahoracadoGame.Palabra);
+        }
+
+        [TestMethod]
+        public void IngresCaracoles_EsperoLongitud9()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            string palabra = "caracoles";
+            ahoracadoGame.AsignarPalabra(palabra);
+
+            Assert.AreEqual(9, ahoracadoGame.CantLetras());
+        }
+
+        [TestMethod]
+        public void IngresoLetraIncorrecta_devuelveLetraIncorrecta()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass(2);
+            string palabra = "caracoles";
+            string letra = "k";
+            ahoracadoGame.AsignarPalabra(palabra);
+            Assert.AreEqual("letra incorrecta", ahoracadoGame.IngresarLetra(letra));
+        }
+
+
+        [TestMethod]
+        public void IngresoLetraO_CorrectoPosicion6()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            string letra = "o";
+            Assert.AreEqual("-----o---", ahoracadoGame.IngresarLetra(letra));
+        }
+
+        [TestMethod]
+        public void PalabraAdivinada_todosGuiones()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            Assert.AreEqual("---------", ahoracadoGame.PalabraAdivinada);
+        }
+
+        [TestMethod]
+        public void IngresoLetraA_CorrectoPosicion6()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            string letra = "a";
+            Assert.AreEqual("-a-a-----", ahoracadoGame.IngresarLetra(letra));
+        }
+        [TestMethod]
+        public void IngresarLetraErronea_ContarErrores()
+        {
+
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass(2);
+            ahoracadoGame.AsignarPalabra("cara");
+            ahoracadoGame.IngresarLetra("k");
+            ahoracadoGame.IngresarLetra("t");
+            ahoracadoGame.IngresarLetra("p");
+            Assert.AreEqual(3, ahoracadoGame.ContErrores);
+        }
+
+        [TestMethod]
+        public void IngresarLaSegundaLetra_estaBien()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            ahoracadoGame.IngresarLetra("a");
+            ahoracadoGame.IngresarLetra("l");
+
+            Assert.AreEqual("-a-a--l--", ahoracadoGame.IngresarLetra("l"));
+        }
+
+
+        [TestMethod]
+        public void IngresarUltimaLetra_ganasteWinner()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            ahoracadoGame.IngresarLetra("c");
+            ahoracadoGame.IngresarLetra("a");
+            ahoracadoGame.IngresarLetra("r");
+            ahoracadoGame.IngresarLetra("o");
+            ahoracadoGame.IngresarLetra("l");
+            ahoracadoGame.IngresarLetra("s");
+            Assert.AreEqual("Palabra adivinada", ahoracadoGame.IngresarLetra("e"));
+        }
+
+        [TestMethod]
+        public void Hiciste6errores_Perdiste()
+        {
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass(2);
+            ahoracadoGame.AsignarPalabra("cara");
+            ahoracadoGame.IngresarLetra("f");
+            ahoracadoGame.IngresarLetra("p");
+            ahoracadoGame.IngresarLetra("y");
+            ahoracadoGame.IngresarLetra("u");
+            ahoracadoGame.IngresarLetra("q");
+            Assert.AreEqual("Perdiste, queso", ahoracadoGame.IngresarLetra("b"));
+        }
+
+        [TestMethod]
+        public void CalcularElTiempo_verdadero()
+        {
+            DateTime inicioJuego = new DateTime(2019, 6, 3, 18, 00, 00); // 18.00
+            DateTime finJuego = new DateTime(2019, 6, 3, 18, 10, 10); // 18.10
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            Assert.AreEqual(610, ahoracadoGame.CalcularTiempoJuego(inicioJuego, finJuego));
+
+        }
+
+        [TestMethod]
+        public void ArriesgarPalabraMierda_verdadero()
+        {
+
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            ahoracadoGame.AsignarPalabra("mierda");
+            Assert.AreEqual("arriesgaste bien, campeon", ahoracadoGame.ArriesgarPalabra("mierda"));
+
+        }
+
+        [TestMethod]
+        public void ArriesgarPalabraPerdedora_PerdisteRey()
+        {
+
+            AhorcadoClass.AhorcadoClass ahoracadoGame = new AhorcadoClass.AhorcadoClass();
+            ahoracadoGame.AsignarPalabraRandom();
+            Assert.AreEqual("te apuraste y te la mandaste, perdedor", ahoracadoGame.ArriesgarPalabra("cacona"));
+
+        }
+
+
+    }
+
+
+}
+
